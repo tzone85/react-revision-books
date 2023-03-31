6a447927acff02e00f510a98f181203e55637059
Thando Mini <tzone85@gmail.com>
2023-03-31 03:26:18 +0200

    removed some unused files. Added styling to our book components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {
+    return (
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    )
+}
+const Book = () => {
+    return (
+        <article className="book">
+            <Image />
+            <Title />
+            <Author />
+        </article>
+        );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg" alt="Dog Man" />
+
+const Title = () => {
+    return (
+        <h2>Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man 11): From the Creator of Captain Underpants</h2>
+    );
+}
+
+const Author = () => <h4>Dav Pilkey</h4>
+
+// const Greeting = () => {
+//     return React.createElement(
+//         'div',
+//         {},
+//         React.createElement('h2', {}, 'Hello world2')
+//     );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList/>)
