9ddf9f5804732c2fab06dbbe271fcb18d60e7395
Thando Mini <tzone85@gmail.com>
2023-04-01 11:34:49 +0200

    simple list and accessing items using js map() function

--- src/index.js
+++ src/index.js
@@ -3,37 +3,31 @@ import ReactDOM from "react-dom/client"
 
 import './index.css';
 
-const firstBook = {
-    title : "Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man 11): From the Creator of Captain Underpants",
-    author : "Dav Pilkey",
-    img : "./images/book1.jpg"
-}
+const books = [
+    {
+        title : "Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man 11): From the Creator of Captain Underpants",
+        author : "Dav Pilkey",
+        img : "./images/book1.jpg"
+    },
 
-const secondBook = {
-    title : "Outlive: The Science and Art of Longevity",
-    author : "Peter Attia MD",
-    img : "https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71IJiOOyb1L._AC_UL600_SR600,400_.jpg"
-}
+    {
+        title : "Outlive: The Science and Art of Longevity",
+        author : "Peter Attia MD",
+        img : "https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71IJiOOyb1L._AC_UL600_SR600,400_.jpg"
+    }
+];
+
+const names = ['Mncedi', 'Peter', 'Paul'];
+const newNames = names.map((name) =>{
+    return <h1>{name}</h1>
+});
+
+console.log('NewNames', newNames);
 
 const BookList = () => {
     return (
         <section className="booklist">
-            <Book 
-                author={firstBook.author} 
-                title={firstBook.title} 
-                img={firstBook.img} 
-                >
-                
-                <p>Deserunt velit occaecat non ea voluptate exercitation adipisicing cupidatat adipisicing eu cupidatat magna est.</p>
-                <button>click me </button>
-                
-                </Book>
-            <Book  
-                author={secondBook.author} 
-                title={secondBook.title} 
-                img={secondBook.img}
-                
-                />
+            {newNames}
         </section>
     )
 }
